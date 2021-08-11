79e7243de44727bd070c1229aa6980b3675d416b
Nithin Joy <56895565+nithin-j@users.noreply.github.com>
2021-08-10 23:08:26 -0500

    added functionality to startgame

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,28 @@ let firstCard = 8
 let secondCard = 11
 let hasBlackJack = false
 let isAlive = true
-let message = ''
+let message = 'Do you wanna give it a shot?'
 
 let sum = firstCard + secondCard
 
-let continueGame = document.getElementById('btn-continue')
+let continueGame = document.getElementById('btn-new-card')
 let saveGame = document.getElementById('btn-save')
 let startGame = document.getElementById('btn-start')
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+let cardsEl= document.getElementById('cards-el')
 
+
+messageEl.textContent = message
 
 function fnStartGame(){
     if (sum <= 20){
-        message = 'Do you want to draw another card?)'
+        message = 'Do you want to draw another card?'
     } else if (sum === 21){
-        message = 'Hoooray!!!!!!, You won the jack pot, party'
+        message = "Congradulations, That's a BlackJack"
         hasBlackJack = true
     } else{
-        message = 'Saaaad, You are out of the game, better luck next time'
+        message = 'You are out of the game, better luck next time.'
         isAlive = false 
     }
     
@@ -28,6 +33,10 @@ function fnStartGame(){
     }
     saveGame.hidden = false
     startGame.hidden = true
+
+    messageEl.textContent = message
+    sumEl.textContent = `${sumEl.textContent}: ${sum}`
+    cardsEl.textContent = `${cardsEl.textContent}: ${firstCard}, ${secondCard}`
 }
 
 function fnSaveGame(){
